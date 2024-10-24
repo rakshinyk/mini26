406a9e6f856b0ea26e794cb3b3b9925c1fd1eb8e
rakshitha <rakshinyks@gmail.com>
2024-10-24 07:54:07 -0700

    commit

--- python108.py
+++ python108.py
@@ -1,8 +1,8 @@
-import requests
+import bs4
 from bs4 import BeautifulSoup as bs
+import requests
 
-link = "https://www.flipkart.com/watches/pr?sid=r18"
-url = requests.get(link)
-#soup = bs(url.text)
-#elements = soup.find_all("div",class_="_1AtVbE col-12-12")
+link='https://www.flipkart.com/search?q=tv&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_8_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_8_0_na_na_na&as-pos=8&as-type=TRENDING&suggestionId=tv&requestId=9c9fa553-b7e5-454b-a65b-bbb7a9c74a29'
 
+page = requests.get(link)
+print(page.content)
